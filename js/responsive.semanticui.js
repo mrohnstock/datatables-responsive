0568b3e4059b76104764380d165227d212c09075
Mathias Rohnstock <m.rohnstock@googlemail.com>
2016-05-20 09:18:01 +0200

    DataTables-Responsive v2.1.0

--- js/responsive.semanticui.js
+++ js/responsive.semanticui.js
@@ -5,7 +5,7 @@
 (function( factory ){
 	if ( typeof define === 'function' && define.amd ) {
 		// AMD
-		define( ['jquery', 'datatables.net-bs', 'datatables.net-responsive'], function ( $ ) {
+		define( ['jquery', 'datatables.net-se', 'datatables.net-responsive'], function ( $ ) {
 			return factory( $, window, document );
 		} );
 	}
@@ -17,7 +17,7 @@
 			}
 
 			if ( ! $ || ! $.fn.dataTable ) {
-				$ = require('datatables.net-bs')(root, $).$;
+				$ = require('datatables.net-se')(root, $).$;
 			}
 
 			if ( ! $.fn.dataTable.Responsive ) {
@@ -39,15 +39,11 @@ var DataTable = $.fn.dataTable;
 var _display = DataTable.Responsive.display;
 var _original = _display.modal;
 var _modal = $(
-	'<div class="modal fade dtr-bs-modal" role="dialog">'+
-		'<div class="modal-dialog" role="document">'+
-			'<div class="modal-content">'+
-				'<div class="modal-header">'+
-					'<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>'+
-				'</div>'+
-				'<div class="modal-body"/>'+
-			'</div>'+
+	'<div class="ui modal" role="dialog">'+
+		'<div class="header">'+
+			'<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>'+
 		'</div>'+
+		'<div class="content"/>'+
 	'</div>'
 );
 
@@ -59,18 +55,18 @@ _display.modal = function ( options ) {
 		else {
 			if ( ! update ) {
 				if ( options && options.header ) {
-					_modal.find('div.modal-header')
+					_modal.find('div.header')
 						.empty()
-						.append( '<h4 class="modal-title">'+options.header( row )+'</h4>' );
+						.append( '<h4 class="title">'+options.header( row )+'</h4>' );
 				}
 
-				_modal.find( 'div.modal-body' )
+				_modal.find( 'div.content' )
 					.empty()
 					.append( render() );
 
 				_modal
 					.appendTo( 'body' )
-					.modal();
+					.modal('show');
 			}
 		}
 	};
